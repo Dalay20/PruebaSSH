373c0a9676517f217a6019cbbfdc7478c91aa235
Diego A <dalay@espol.edu.ec>
2025-05-05 12:47:52 -0500

    Primer commit del proyecto Java en NetBeans

---
+++ src/main/java/com/mycompany/prueba01/Prueba01.java
@@ -0,0 +1,18 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.prueba01;
+
+/**
+ *
+ * @author CDHTN
+ */
+public class Prueba01 {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+        System.out.println("PROBAR GIT DESDE NETABESANS");
+        
+    }
+}
